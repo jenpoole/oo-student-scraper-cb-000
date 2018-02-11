e0a24e2d39dd0a641575592534e674f5a0355b6b
Jen Poole <contact.vandepool@gmail.com>
2018-02-11 23:54:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,8 +28,35 @@ class Scraper
     students
   end
 
-  def self.scrape_profile_page(profile_url)
 
+  # is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student
+  def self.scrape_profile_page(profile_url)
+    profile_html = Nokogiri::HTML(open(profile_url))
+
+    #  The return value of this method should be a hash in which the key/value pairs describe an individual student.
+    # Some students don't have a twitter or some other social link; be sure to be able to handle that.
+    student = {}
+
+    links = profile_html.css(".social-icon-container").children.css("a").map { |element| element.attribute('href').value}
+
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+
+    student[:profile_quote] = profile_html.css(".profile-quote").text if profile_html.css(".profile-quote")
+
+    student[:bio] = profile_html.css(".bio-content.content-holder .description-holder p").text if profile_html.css(".bio-content.content-holder .description-holder p")
+
+    student
   end
+
 
 end
